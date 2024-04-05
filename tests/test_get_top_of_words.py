8fa0ad6083752e32e26375d76236201d904f0a56
luiqor <vohnenna@gmail.com>
2024-04-05 13:05:51 +0300

    get_top_of_words added with parametrization

---
+++ tests/test_get_top_of_words.py
@@ -0,0 +1,15 @@
+import pytest
+from main import get_top_of_words
+
+
+@pytest.mark.parametrize(
+    "word_count, number_of_words, expected_output",
+    [
+        ({"hello": 2, "world": 1}, 2, [("hello", 2), ("world", 1)]),
+        ({"one": 2, "two": 1, "three": 3}, 2, [("three", 3), ("one", 2)]),
+        ({"one": 2, "two": 1, "three": 3}, 1, [("three", 3)]),
+        ({}, 10, []),
+    ],
+)
+def test_get_top_of_words(word_count, number_of_words, expected_output):
+    assert get_top_of_words(word_count, number_of_words) == expected_output
