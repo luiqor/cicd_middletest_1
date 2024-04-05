408e38c0e728db7ddb7fbd24ae408f25549ab578
luiqor <vohnenna@gmail.com>
2024-04-05 13:17:12 +0300

    get_top_of_words function to return a tuple & naming refactored

--- main.py
+++ main.py
@@ -11,17 +11,17 @@ def count_words(words: list) -> dict:
     return word_count
 
 
-def get_top_of_words(word_count: dict, number_of_words: int = 10) -> dict:
+def get_top_of_words(word_count: dict, number_of_words: int = 10) -> tuple:
     sorted_word_count = sorted(word_count.items(),
                                key=lambda x: x[1],
                                reverse=True)
-    top_10_words = sorted_word_count[:number_of_words]
-    return top_10_words
+    top_of_words = sorted_word_count[:number_of_words]
+    return top_of_words
 
 
-def write_to_file(output_file: str, top_10_words: dict) -> None:
+def write_to_file(output_file: str, top_of_words: tuple) -> None:
     with open(output_file, 'w') as output:
-        for word, count in top_10_words:
+        for word, count in top_of_words:
             output.write(f"{word}: {count}\n")
 
 
@@ -31,9 +31,9 @@ def main(input_file, output_file):
     words = text.split()
     word_count = count_words(words)
 
-    top_10_words = get_top_of_words(word_count, 10)
+    top_of_words = get_top_of_words(word_count, 10)
 
-    write_to_file(output_file, top_10_words)
+    write_to_file(output_file, top_of_words)
 
 
 if __name__ == '__main__':
